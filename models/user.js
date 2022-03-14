fec2d43b90cd80bcba700b6583c19fe2431bf237
adamcg123 <adam.Gondorchin26@gmail.com>
2022-03-14 16:07:50 -0500

    models are done

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { Schema, model, types } = require('mongoose')
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+const user = model('User', UserSchema);
+
+
+module.exports = User;
